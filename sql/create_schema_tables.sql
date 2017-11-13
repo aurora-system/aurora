7f150cba6fb040e501ec99b953d11d5161b576b4
user <user@DESKTOP-FVNNECT>
2017-11-14 00:31:43 +0800

    Added Customer and Container tables.

--- sql/create_schema_tables.sql
+++ sql/create_schema_tables.sql
@@ -1,8 +1,20 @@
 CREATE SCHEMA `aurora` ;
 USE aurora;
 
--- This is a sample change
+-- CUSTOMER
+CREATE TABLE `aurora`.`customer` (
+  `customer_id` INT NOT NULL AUTO_INCREMENT,
+  `type` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `address` VARCHAR(255) NULL,
+  `contact_name` VARCHAR(45) NULL,
+  `main_number` VARCHAR(45) NULL,
+  `alternate_number` VARCHAR(45) NULL,
+  `email_address` VARCHAR(45) NULL,
+  PRIMARY KEY (`customer_id`))
+COMMENT = 'Customer details';
 
+-- ORDER
 CREATE TABLE `aurora`.`order` (
   `order_id` INT NOT NULL,
   `customer_id` INT NOT NULL,
@@ -15,6 +27,7 @@ CREATE TABLE `aurora`.`order` (
   PRIMARY KEY (`order_id`),
   UNIQUE INDEX `order_id_UNIQUE` (`order_id` ASC));
   
+-- DEBT
 CREATE TABLE `aurora`.`debt` (
   `debt_id` INT NOT NULL,
   `customer_id` INT NOT NULL,
@@ -23,7 +36,8 @@ CREATE TABLE `aurora`.`debt` (
   `created_at` DATETIME NOT NULL DEFAULT NOW(),
   PRIMARY KEY (`debt_id`))
   UNIQUE INDEX `debt_id_UNIQUE` (`debt_id` ASC);
-  
+
+-- PAYMENT
 CREATE TABLE `aurora`.`payment` (
   `payment_id` INT NOT NULL,
   `customer_id` INT NOT NULL,
@@ -32,3 +46,11 @@ CREATE TABLE `aurora`.`payment` (
   `created_at` DATETIME NOT NULL DEFAULT NOW(),
   PRIMARY KEY (`payment_id`),
   UNIQUE INDEX `payment_id_UNIQUE` (`payment_id` ASC));
+
+-- CONTAINER
+CREATE TABLE `aurora`.`container` (
+  `customer_id` INT NOT NULL,
+  `status` VARCHAR(45) NOT NULL,
+  `round_count` INT NULL,
+  `slim_count` INT NULL,
+  `date` DATETIME NULL);
