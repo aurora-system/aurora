85f1672cba5957939d84de383b7a044bc6a8366d
Carlos Jhunie Pangilinan Jr <cjpangilinan@gmail.com>
2018-01-02 12:45:13 -0800

    Fixed @Autowired error. Removed bean from servlet-context.xml

--- src/main/java/com/spring/aurora/dao/CustomerDaoImpl.java
+++ src/main/java/com/spring/aurora/dao/CustomerDaoImpl.java
@@ -6,12 +6,13 @@ import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Repository
 public class CustomerDaoImpl implements CustomerDao {
 	
 	private static final Logger logger = LoggerFactory.getLogger(CustomerDaoImpl.class);
--- src/main/java/com/spring/aurora/dao/OrderDaoImpl.java
+++ src/main/java/com/spring/aurora/dao/OrderDaoImpl.java
@@ -1,4 +1,31 @@
 package com.spring.aurora.dao;
 
-public class OrderDaoImpl {
+import com.spring.aurora.model.Order;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order insert(Order order) {
+        return null;
+    }
+
+    @Override
+    public Order update(Order order) {
+        return null;
+    }
+
+    @Override
+    public List<Order> findAllByCustomerId(int customerId) {
+        return null;
+    }
+
+    @Override
+    public List<Order> findAllByDeliveryReceiptNumber(int drNumber) {
+        return null;
+    }
 }
