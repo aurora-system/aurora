7128498e614aad0c696556527afac282846b4b5a
Carlos Jhunie Pangilinan <carlos_jhunie.pangilinan@telus.com>
2018-05-31 01:23:54 -0700

    alter sql stmt

--- sql/alter_statements.sql
+++ sql/alter_statements.sql
@@ -1,3 +1,13 @@
 -- payments table
 ALTER TABLE `aurora`.`payment`
 ADD COLUMN `wh_tax` DECIMAL(10,2) NOT NULL AFTER `amount`;
+
+
+-- orders table
+ALTER TABLE `aurora`.`orders`
+DROP COLUMN `round_buy_count`,
+DROP COLUMN `slim_buy_count`;
+
+ALTER TABLE `aurora`.`orders`
+ADD COLUMN `slim_buy_count` INT(11) NOT NULL DEFAULT '0' AFTER `cont_round_count`,
+ADD COLUMN `round_buy_count` INT(11) NOT NULL DEFAULT '0' AFTER `slim_buy_count`;
